783a17939dfe38985df384244d1bbe590ed142dc
Will Hilton <wmhilton@gmail.com>
2017-12-16 10:18:09 -0500

    Yeaaaaahh!

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+// Rollup plugins
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/wwsw.js',
+    format: 'es'
+  },
+  plugins: [
+    resolve({
+      module: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+}
